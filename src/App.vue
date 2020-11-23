7386e9286b740f890e924a91be3f6dee6d9e6231
Marc Downie <marcdownie3@hotmail.com>
2020-11-23 20:59:11 +0000

    finished mvp, wish i could have done extensions

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,46 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header id="header">ToDo's</header>
+    <form v-on:submit.prevent="saveNewTodo">
+      <label id="text-box" for="new-todo">Add to list:</label>
+      <input id="new-todo" type="text" v-model="newTodo">
+      <input type="submit" value="save">
+    </form>
+    <ul>
+      <li id="list" v-for="(item, index) in todos" v-bind:key="index">
+        <span>{{item.name}}</span>
+        <span v-if="item.completed">Complete!</span>
+        <button id="button" v-if="!item.completed" v-on:click="finishTodo(index)">Complete task</button>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return{
+      todos:[
+        {name: "Shopping", completed: false},
+        {name: "Clean house", completed: false},
+        {name: "Feed cats", completed: false},
+        {name: "Buy Christmas presents!", completed: false},
+        {name: "WIN THE LOTTERY", completed: false}
+      ],
+      newTodo: ""
+    }
+  },
+  methods: {
+    saveNewTodo: function(){
+      this.todos.push({
+        name: this.newTodo,
+        completed: false
+      });
+      this.newTodo = "";
+    },
+    finishTodo: function(index){
+      this.todos[index].completed = true;
+    }
   }
 }
 </script>
@@ -19,10 +48,24 @@ export default {
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 20px;
+}
+#header {
+  margin-left: 20px;
+  margin-bottom:30px;
+  font-family: Avenir, Helvetica;
+  font-style: bold;
+  font-size: 40px;
+  color: black
+}
+#list {
+  padding-top: 20px;
+}
+#button {
+  margin-left: 10px;
+}
+#text-box {
+  margin-left: 25px;
 }
 </style>
